21bf3ffb670dc92963db41420d578397e7dda24b
élOOm <contato@eloom.com.br>
2024-07-18 00:21:35 -0300

    First commit

--- app/code/local/Eloom/BlingV3/Model/Observer.php
+++ app/code/local/Eloom/BlingV3/Model/Observer.php
@@ -2,18 +2,20 @@
 
 ##eloom.licenca##
 
+use Eloom\SdkBling\Enum\TipoPessoa;
+use Eloom\SdkBling\Model\Request\Nfe;
+
 class Eloom_BlingV3_Model_Observer {
-	
+
 	private $logger;
-	
+
 	/**
 	 * Initialize resource model
 	 */
-	protected function _construct() {
+	public function __construct() {
 		$this->logger = Eloom_Bootstrap_Logger::getLogger(__CLASS__);
-		parent::_construct();
 	}
-	
+
 	public function addNfeMassAction($observer) {
 		$block = $observer->getEvent()->getBlock();
 		if ($block instanceof Mage_Adminhtml_Block_Widget_Grid_Massaction) {
@@ -25,10 +27,52 @@ class Eloom_BlingV3_Model_Observer {
 			}
 		}
 	}
-	
+
 	public function processCallback($observer) {
 		$code = $observer->getEvent()->getCode();
-		
-		$this->logger->info(sprintf("Processando callback. Código [%s] - Estado [%s].", $code));
+		$this->logger->info(sprintf("Processando callback. Código [%s].", $code));
+
+		//$dataOperacao = Mage::getModel('core/date')->date('Y-m-d H:i:s', strtotime(time()));
+		$nfe = Nfe::of();
+		$nfe->setTipo(1);
+		$nfe->setNumero(6541);
+		$nfe->setDataEmissao(new DateTime());
+		$nfe->setDataOperacao(new DateTime());
+
+		$contato = $nfe->getContato();
+		$contato->setId('123456');
+		$contato->setNome('Rodrigo Sanders');
+		$contato->setTipoPessoa(TipoPessoa::FISICA);
+		$contato->setContribuinte(2);
+		$contato->setTelefone('5198038165');
+		$contato->setRg('1078101928');
+		$contato->setEmail('paulo.sanders@gmail.com');
+
+		$endereco = $contato->getEndereco();
+		$endereco->setEndereco('Rua Formosa');
+		$endereco->setNumero('110');
+		$endereco->setComplemento('Casa');
+		$endereco->setCep('92480-000');
+		$endereco->setBairro('Califórnia');
+		$endereco->setMunicipio('Nova Santa Rita');
+		$endereco->setUf('RS');
+		$endereco->setPais('Brasil');
+		$contato->setEndereco($endereco);
+
+		$nfe->setContato($contato);
+
+		$transporte = $nfe->getTransporte();
+		$transporte->setFretePorConta(0);
+		$transporte->setFrete(20);
+
+		$veiculo = $transporte->getVeiculo();
+		$veiculo->setPlaca('LDO-2373');
+		$veiculo->setUf('RS');
+		$veiculo->setMarca('Volvo');
+
+		$transporte->setVeiculo($veiculo);
+		$nfe->setTransporte($transporte);
+
+		$this->logger->info($nfe->jsonSerialize());
 	}
 }
